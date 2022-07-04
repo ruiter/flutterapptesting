5d54d8d67409e3673d7429f198deb4dda45d7bb1
ruiter <ruitercomp@gmail.com>
2022-07-04 20:05:44 -0300

    creating a image and lib folders

---
+++ lib/learn_flutter_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({Key? key}) : super(key: key);
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learn Flutter'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Column(
+        children: [Image.asset('images/einstein.jpg')],
+      ),
+    );
+  }
+}
